47115a7f203955430940bebaa50c1cfd8656f50d
Graham Parker <frymyox@gmail.com>
2018-04-28 17:36:21 +1200

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,14 @@
+import unittest
+import tests.test_file_reader
+import tests.test_database_excel
+import tests.test_charts
+
+loader = unittest.TestLoader()
+all_my_tests = unittest.TestSuite()
+
+all_my_tests.addTests(loader.loadTestsFromModule(tests.test_file_reader))
+all_my_tests.addTests(loader.loadTestsFromModule(tests.test_database_excel))
+all_my_tests.addTests(loader.loadTestsFromModule(tests.test_charts))
+
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(all_my_tests)
